f9f80abdf51eb90554a2f14af70c028fd44390eb
joaovian06 <joaovitor_vian@hotmail.com>
2020-05-08 13:14:23 -0300

    green: somando multiplos

--- project-euler/soma-multiplos.rb
+++ project-euler/soma-multiplos.rb
@@ -3,8 +3,7 @@ class Multiples
   def find_sum_multiples(a, b, limit)
     sum = 0
     (1...limit).each do |num|
-      puts sum += num if num % a == 0
-      puts sum += num if num % b == 0
+      sum += num if (num % a == 0) || (num % b == 0)
     end
     sum
   end
@@ -14,12 +13,13 @@ end
 mult = Multiples.new
 
 # teste de sucesso
-# dado um limite e dois numeros naturais 3 e 5 abaixo de 10 quero que a soma seja 23
+# dado um limite(10) e dois numeros naturais(3 e 5) quero que a soma dos multiplos seja 23
 puts mult.find_sum_multiples(3, 5, 10) == 23
 
 # teste de falha
-# dado um conjunto de multiplos de 3 e 4 abaixo de 10 quero que a soma seja diferente de 23
+# dado um limite(10) e dois numeros naturais(3 e 4) quero que a soma dos multiplos seja diferente de 23
 puts mult.find_sum_multiples(3, 4, 10) != 23
 
+# dado um limite(16) e dois numeros naturais(3 e 5) quero que a soma dos multiplos seja 60
 puts mult.find_sum_multiples(3, 5, 16) == 60
-#puts mult.find_sum_multiples(3, 5, 1000)
+
