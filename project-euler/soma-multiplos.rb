cf8624cc798f2170ad6d43dec140ba740ce6799e
joaovian06 <joaovitor_vian@hotmail.com>
2020-05-08 08:29:45 -0300

    green: percorrendo valores dentro do limite

--- project-euler/soma-multiplos.rb
+++ project-euler/soma-multiplos.rb
@@ -1,6 +1,9 @@
 # IMPLEMENTACAO
 class Multiples
-  def sum_multiples(num_1, num_2, limit)
+  def find_sum_multiples(num_1, num_2, limit)
+    (1...limit).each do |check|
+      print check
+    end
   end
 end
 
@@ -9,8 +12,8 @@ mult = Multiples.new
 
 # teste de sucesso
 # dado um conjunto de multiplos de 3 e 5 ate o 10 quero que a soma seja 23
-mult.sum_multiples(3, 5, 10) == 23
+mult.find_sum_multiples(3, 5, 10) == 23
 
 # teste de falha
 # dado um conjunto de multiplos de 3 e 4 ate o 10 quero que a soma seja diferente de 23
-mult.sum_multiples(3, 4, 10) != 23
+mult.find_sum_multiples(3, 4, 10) != 23
