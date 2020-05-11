4c8f75c0d8eaef6694aa42e9e1f8b9f194f46eaa
joaovian06 <joaovitor_vian@hotmail.com>
2020-05-11 11:08:50 -0300

    descricao de primeiro teste melhorada

--- project-euler/soma-multiplos/spec/soma_multiplos_spec.rb
+++ project-euler/soma-multiplos/spec/soma_multiplos_spec.rb
@@ -1,7 +1,7 @@
 require '../soma-multiplos'
 
 describe 'soma-multiplos' do
-  context 'when passed 3, 5, 10' do # elaborar descricao mais especifica
+  context 'when passed a limit (10) and two natural numbers (3 and 5)' do
     it 'returns 23' do
       mult = Multiple.new
       expect(mult.find_sum_multiples(3, 5, 10)).to eq 23
