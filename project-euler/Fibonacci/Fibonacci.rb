b1759411f0aade8e441fa2ddfe3dc3bfefd49ad6
joaovian06 <joaovitor_vian@hotmail.com>
2020-05-12 11:34:17 -0300

    renomando arquivos e pastas

--- project-euler/Fibonacci/Fibonacci.rb
+++ project-euler/Fibonacci/Fibonacci.rb
@@ -5,13 +5,13 @@ class Fibonacci
     @fibonacci_nums = Array.new
     @limit = limit
   end
+  def generate
+    # nao pode ser maior que 89(limit)
+  end
   def find_even_valued
     generate
     # achar os valores pares dentro do meu array
     # retorna um array com os pares
-  end
-  def generate
-    # nao pode ser maior que 89(limit)
   end
   def sum_even_valued
     # soma valores pares retornado do finc_even_valued
