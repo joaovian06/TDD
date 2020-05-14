f4430e4983cf09afdce8d955cc7e4b2d39c00fc0
joaovian06 <joaovitor_vian@hotmail.com>
2020-05-14 11:18:06 -0300

    red: corrigindo nome de instancia e chamando metodo do objeto

--- project-euler/largest_prime_factor/largest_prime_factor.rb
+++ project-euler/largest_prime_factor/largest_prime_factor.rb
@@ -1,7 +1,6 @@
 class LargestPrimeFactor
   def get_primes(number)
     all_primes = []
-    my_number = number
 
   end
 end
--- project-euler/largest_prime_factor/spec/largest_prime_factor_spec.rb
+++ project-euler/largest_prime_factor/spec/largest_prime_factor_spec.rb
@@ -3,8 +3,8 @@ describe 'Largest prime factor' do
   describe 'get_primes' do
     context 'given a value' do
       it 'is expected a return containing all prime values ​​within the given value' do
-        my_prime_factor = LargesPrimeFactor.new
-        expect(get_primes(30)).to eq [2,3,5,7,11,13,17,19,23,29]
+        my_prime_factor = LargestPrimeFactor.new
+        expect(my_prime_factor.get_primes(30)).to eq [2,3,5,7,11,13,17,19,23,29]
       end
       # it '' do
       #   expect(greatest_factor(13195)).to eq 29
