aae526c91c8ecac2bb3befbb5126c86583331240
joaovian06 <joaovitor_vian@hotmail.com>
2020-05-15 10:25:44 -0300

    green: segundo teste passando

--- project-euler/largest_prime_factor/largest_prime_factor.rb
+++ project-euler/largest_prime_factor/largest_prime_factor.rb
@@ -1,11 +1,26 @@
 require 'Prime'
 class LargestPrimeFactor
+  attr_reader :primes
   def get_primes(limit)
     primes = []
     Prime.each(limit) do |num|
       primes << num
     end
   end
-  def get_factors(number)
+
+  def get_factors(limit, my_array)
+    factor = []
+    factored = limit
+    my_array.each do |num|
+      if factored % num == 0
+        factored = factored / num
+        factor << num
+      else
+        next
+      end
+    end
+    factor
   end
+  # def greatest_factor
+  # end
 end
--- project-euler/largest_prime_factor/spec/largest_prime_factor_spec.rb
+++ project-euler/largest_prime_factor/spec/largest_prime_factor_spec.rb
@@ -10,13 +10,17 @@ describe 'Largest prime factor' do
     describe 'get_factors' do
       it 'is expected a return containing all prime factor that the given number is divisible for' do
         my_prime_factor = LargestPrimeFactor.new
-        expect(my_prime_factor.get_factors(30)).to eq [2,3,5]
+        return_primes = my_prime_factor.get_primes(30)
+        expect(my_prime_factor.get_factors(30,return_primes)).to eq [2,3,5]
       end
     end
-# describe 'greatest_factor' do
-#   it '' do
-#     expect(greatest_factor(13195)).to eq 29
-#   end
-# end
+    # describe 'greatest_factor' do
+    #   it 'given a array is expected a return containing the greatest value' do
+    #     my_prime_factor = LargestPrimeFactor.new
+    #     return_primes = my_prime_factor.get_primes(30)
+    #     return_factor = my_prime_factor.get_factors(30,return_primes)
+    #     expect(my_prime_factor.greatest_factor(return_factor)).to eq 5
+    #   end
+    # end
   end
 end
