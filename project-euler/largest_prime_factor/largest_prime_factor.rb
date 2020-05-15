2824919de6baa86fa1f234f9f26716106ee8f83b
joaovian06 <joaovitor_vian@hotmail.com>
2020-05-15 13:50:02 -0300

    refactor: get_factor e get_primes

--- project-euler/largest_prime_factor/largest_prime_factor.rb
+++ project-euler/largest_prime_factor/largest_prime_factor.rb
@@ -1,26 +1,36 @@
 require 'Prime'
+# find the largest prime factor to a given number
 class LargestPrimeFactor
   def get_primes(primes_limit)
-    primes = []
-    Prime.each(primes_limit) do |num|
-      primes << num
-    end
+    Prime.each(primes_limit).to_a
   end
 
   def get_factors(to_be_factored, primes_array)
-    factor = []
-    factored = to_be_factored
-    primes_array.each do |num|
-      if factored % num == 0
-        factored = factored / num
-        factor << num
-      else
-        next
-      end
-    end
-    factor
+    primes_array.map do |num|
+      next unless (to_be_factored % num).zero?
+
+      to_be_factored /= num
+      num
+    end.compact
   end
+
+  # def get_factors(to_be_factored, primes_array)
+  #   factor = []
+  #   primes_array.each do |num|
+  #     next unless (to_be_factored % num).zero?
+
+  #     to_be_factored /= num
+  #     factor << num
+  #   end
+  #   factor
+  # end
+
   def greatest_factor(factor_array)
-    factor_array.sort.last
+    factor_array.max
   end
 end
+
+a = LargestPrimeFactor.new
+print return_prime = a.get_primes(600_851_475_143)
+#return_factor = a.get_factors(600_851_475_143, return_prime)
+#puts a.greatest_factor(return_factor)
