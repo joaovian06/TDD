a3a8f0b9336f86f04aab915fba9d9f8ff3a9841e
joaovian06 <joaovitor_vian@hotmail.com>
2020-05-18 14:51:39 -0300

    refactor: renomeando o  metodo para adicionar strings

--- project-euler/String-Calculator/spec/string_calculator_spec.rb
+++ project-euler/String-Calculator/spec/string_calculator_spec.rb
@@ -4,15 +4,15 @@ describe 'String Calculator' do
     describe 'add_string' do
       it 'given a empty string input, return must be equal 0' do
         string_calc = StringCalculator.new
-        expect(string_calc.add_string('')).to eq 0
+        expect(string_calc.add('')).to eq 0
       end
       it 'given a string containing a single value, return must be equal to the value' do
         string_calc = StringCalculator.new
-        expect(string_calc.add_string('1')).to eq 1
+        expect(string_calc.add('1')).to eq 1
       end
       it 'given a string containing tow values, return must be equal the sum of these values ' do
         string_calc = StringCalculator.new
-        expect(string_calc.add_string('1,2')).to eq 3
+        expect(string_calc.add('1,2')).to eq 3
       end
     end
   end
--- project-euler/String-Calculator/string_calculator.rb
+++ project-euler/String-Calculator/string_calculator.rb
@@ -1,5 +1,5 @@
 class StringCalculator
-  def add_string(my_string)
+  def add(my_string)
     return 0 if my_string.empty?
     return my_string.to_i unless my_string.include? ','
 
