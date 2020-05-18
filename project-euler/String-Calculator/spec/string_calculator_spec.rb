48bec3070b6ed351b4e50483a19695c67a63fcdd
joaovian06 <joaovitor_vian@hotmail.com>
2020-05-18 13:21:54 -0300

    refactor: melhorando descricao do teste

--- project-euler/String-Calculator/spec/string_calculator_spec.rb
+++ project-euler/String-Calculator/spec/string_calculator_spec.rb
@@ -2,7 +2,7 @@ require '../string_calculator'
 describe 'String Calculator' do
   context 'given a string input containing a number' do
     describe 'add_string' do
-      it 'given a empty string input' do
+      it 'given a empty string input, return must be equal 0' do
         string_calc = StringCalculator.new
         expect(string_calc.add_string('')).to eq 0
       end
