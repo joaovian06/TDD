9f5b9c467ea6e32302a0688bac408bab68442530
joaovian06 <joaovitor_vian@hotmail.com>
2020-05-18 14:34:22 -0300

    green: teste string para calcular passando

--- project-euler/String-Calculator/string_calculator.rb
+++ project-euler/String-Calculator/string_calculator.rb
@@ -2,5 +2,9 @@ class StringCalculator
   def add_string(my_string)
     return 0 if my_string.empty?
     return my_string.to_i unless my_string.include? ','
+
+    if my_string.include? ','
+      my_string.split(',').map! { |string_num| string_num.to_i }.sum
+    end
   end
 end
