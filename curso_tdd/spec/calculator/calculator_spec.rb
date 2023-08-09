d408370d8329a4b600deaca72c8f453abad9be9d
joaovian06 <joaovitor_vian@hotmail.com>
2023-08-08 23:16:23 -0300

    test: introducing `context`

--- curso_tdd/spec/calculator/calculator_spec.rb
+++ curso_tdd/spec/calculator/calculator_spec.rb
@@ -1,17 +1,26 @@
 require 'calculator'
 
 describe Calculator do
-  it 'use sum method for two numbers' do
-    calc = Calculator.new
-    result = calc.sum(5, 2)
+  context '#sum' do
+    it 'with positive numbers' do
+      calc = Calculator.new
+      result = calc.sum(5, 2)
 
-    expect(result).to eq(7)
-  end
+      expect(result).to eq(7)
+    end
 
-  it 'use sum method for two negative numbers' do
-    calc = Calculator.new
-    result = calc.sum(5, -2)
+    it 'with negative and positive numbers' do
+      calc = Calculator.new
+      result = calc.sum(5, -2)
 
-    expect(result).to eq(3)
+      expect(result).to eq(3)
+    end
+
+    it 'with negative numbers' do
+      calc = Calculator.new
+      result = calc.sum(-5, -2)
+
+      expect(result).to eq(-7)
+    end
   end
 end
