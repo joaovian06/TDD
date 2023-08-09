fa9b6558fe46ad409601964cfffb8e18f7a5e8a8
joaovian06 <joaovitor_vian@hotmail.com>
2023-08-09 09:02:54 -0300

    test: conecept subject

--- curso_tdd/spec/calculator/calculator_spec.rb
+++ curso_tdd/spec/calculator/calculator_spec.rb
@@ -1,25 +1,22 @@
 require 'calculator'
 
 describe Calculator do
+  # overiding subject with name calc and described class
+  # subject(:calc) { described_class.new() }
+
   context '#sum' do
     it 'with positive numbers' do
-      calc = Calculator.new
-      result = calc.sum(5, 2)
-
+      result = subject.sum(5, 2)
       expect(result).to eq(7)
     end
 
     it 'with negative and positive numbers' do
-      calc = Calculator.new
-      result = calc.sum(5, -2)
-
+      result = subject.sum(5, -2)
       expect(result).to eq(3)
     end
 
     it 'with negative numbers' do
-      calc = Calculator.new
-      result = calc.sum(-5, -2)
-
+      result = subject.sum(-5, -2)
       expect(result).to eq(-7)
     end
   end
